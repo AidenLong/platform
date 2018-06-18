9395b1799aad635eac04973524dad2925ac2cb0e
小哥哥 <939961241@qq.com>
2018-06-18 13:31:03 +0800

    添加swagger功能

--- platform-restful/src/main/java/com/platform/api/ApiTestController.java
+++ platform-restful/src/main/java/com/platform/api/ApiTestController.java
@@ -2,6 +2,8 @@ package com.platform.api;
 
 import com.platform.annotation.IgnoreAuth;
 import com.platform.utils.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lipengjun
  * @date 2017年11月20日 下午3:29:40
  */
+@Api(value = "ApiTestController|一个用来测试Token的控制器")
 @RestController
 @RequestMapping("/api/test")
 public class ApiTestController {
@@ -19,6 +22,7 @@ public class ApiTestController {
     /**
      * 忽略Token验证测试
      */
+    @ApiOperation(value = "忽略Token验证测试", notes = "无需token也能访问")
     @IgnoreAuth
     @GetMapping("notToken")
     public R notToken() {
--- platform-restful/src/main/java/com/platform/api/ApiUploadController.java
+++ platform-restful/src/main/java/com/platform/api/ApiUploadController.java
@@ -4,6 +4,10 @@ import com.platform.annotation.IgnoreAuth;
 import com.platform.oss.OSSFactory;
 import com.platform.util.ApiBaseAction;
 import com.platform.utils.RRException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author lipengjun
  * @date 2017年11月20日 下午3:29:40
  */
+@Api(value = "ApiUploadController|一个用来文件上传的控制器")
 @RestController
 @RequestMapping("/api/upload")
 public class ApiUploadController extends ApiBaseAction {
@@ -22,8 +27,10 @@ public class ApiUploadController extends ApiBaseAction {
     /**
      * 上传文件
      */
+    @ApiOperation(value = "上传文件", notes = "上传文件")
+    @ApiImplicitParam(paramType = "query", name = "file", value = "文件", required = true, dataType = "MultipartFile")
     @IgnoreAuth
-    @RequestMapping("/upload")
+    @PostMapping("/upload")
     public Object upload(@RequestParam("file") MultipartFile file) throws Exception {
         if (file.isEmpty()) {
             throw new RRException("上传文件不能为空");
